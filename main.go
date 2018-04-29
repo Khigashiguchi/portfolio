81b4aefd21b9aafad7298312b281f28ef446cfcd
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-04-29 17:37:31 +0900

    Revert "use autotls"
    This reverts commit 5037386222704ad197e81b01221538c8db84fa62.

--- main.go
+++ main.go
@@ -2,24 +2,11 @@ package main
 
 import (
 	"github.com/labstack/echo"
-	"golang.org/x/crypto/acme/autocert"
-	"github.com/labstack/echo/middleware"
-	"net/http"
 )
 
 func main() {
 	e := echo.New()
 
-	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
-	e.Use(middleware.Recover())
-	e.Use(middleware.Logger())
-	//e.Static("/", "public")
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, `
-<h1>Welcome to Portfolio!</h1>
-<h3>TLS certificates automatically installed.
-`)
-	})
-	//e.Start(":8080")
-	e.Logger.Fatal(e.StartAutoTLS(":4433"))
+	e.Static("/", "public")
+	e.Start(":8080")
 }
